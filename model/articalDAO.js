5f75d432f6dcb95ee71c571dd8f10572fc85e5c8
ZZA <764004997@qq.com>
2018-09-18 16:17:31 +0800

    完成index路由

--- model/articalDAO.js
+++ model/articalDAO.js
@@ -1,21 +1,41 @@
 const DAO = require('../model/DAO');
 
-class DB{
-    getReconmendEssay(){
-        return DAO('select coverpic,title,articallabel from artical order by pageviews',[])
+class DB {
+    //获取推荐文章数据
+    getReconmendEssay() {
+        return DAO('select coverpic,title,articallabel from artical order by pageviews desc', [])
     }
-    getYangEssay(){
-        return DAO('select coverpic,title,articallabel from artical where articalClassification = "养生堂" order by pageviews desc',[]);
+
+    //获取养生堂文章数据
+    getYangEssay() {
+        return DAO('select coverpic,title,articallabel from artical where articalClassification = "养生堂" order by pageviews desc', []);
     };
-    getDietEssay(){
-        return DAO('select coverpic,title,articallabel from artical where articalClassification = "营养饮食" order by pageviews desc',[]);
+
+    //获取营养饮食文章数据
+    getDietEssay() {
+        return DAO('select coverpic,title,articallabel from artical where articalClassification = "营养饮食" order by pageviews desc', []);
     };
-    getFitEssay(){
-        return DAO('select coverpic,title,articallabel from artical where articalClassification = "健身课堂" order by pageviews desc',[]);
+
+    //获取健身课堂文章数据
+    getFitEssay() {
+        return DAO('select coverpic,title,articallabel from artical where articalClassification = "健身课堂" order by pageviews desc', []);
     };
-    getRelativeEssay(id){
-        return DAO('select title,articallabel,articalClassification,content,contentpic1,contentpic2,contentpic3,contentpic4,contentpic5,artical.userid,autograph from artical,users where artical.userid = users.userid and articalid = ?',[id])
-    }
+
+    //获取点击对应文章数据
+    getRelativeEssay(id) {
+        return DAO('select title,articallabel,articalClassification,content,contentpic1,contentpic2,contentpic3,contentpic4,contentpic5,artical.userid,autograph from artical,users where artical.userid = users.userid and articalid = ?', [id])
+    };
+
+    //获取搜索相关文章数据
+    searchEssay(keys) {
+        return DAO('select coverpic,title,articalClassification,articallabel,pageviews from artical where articallabel like "%' + keys + '%" or articalClassification like "%' + keys + '%" or content like "%' + keys + '%"', [])
+    };
+
+    //获取搜索相关帖子数据
+    searchPost(keys) {
+        return DAO('select postpic1,title,postClassification,postabel,pageviews from post where postabel like "%'+keys+'%" or postClassification like "%'+keys+'%" or postcontent like "%'+keys+'%"', [])
+    };
+
 }
 
 module.exports = new DB();
--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,29 @@ const router = require('koa-router')()
 const articalController = require('../controller/articalController')
 const postController = require('../controller/postController')
 
+//显示首页上的文章数据
 router.get('/', async (ctx, next) => {
-        await articalController.getEssay(ctx,next)
-    }
-  )
-
-router.get('/search', async (ctx, next) => {
-
+        await articalController.getEssay(ctx, next)
     }
 )
-
+//显示搜索之后的文章数据
+router.get('/search/:keys', async (ctx, next) => {
+        await articalController.searchEssay(ctx, next)
+    }
+)
+//显示点击相应链接获取的文章数据
 router.get('/relativeEssay/:articalid', async (ctx, next) => {
-        await articalController.getRelativeEssay(ctx,next)
+        await articalController.getRelativeEssay(ctx, next)
     }
 )
-
+//显示热门帖子推荐信息
 router.get('/hotPost', async (ctx, next) => {
-        await postController.getPost(ctx,next)
+        await postController.getPost(ctx, next)
     }
 )
-
-router.get('/relativePost/:postid', async (ctx, next) => {
-        await postController.getRelativePost(ctx,next)
+//显示点击相应热门帖子获取的数据
+router.get('/hotPost/relativePost/:postid', async (ctx, next) => {
+        await postController.getRelativePost(ctx, next)
     }
 )
 
