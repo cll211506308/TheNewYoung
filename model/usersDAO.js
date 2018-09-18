cbd7381489e18565488daf6827d5f796adb7c7b6
chenglili <2877453844@qq.com>
2018-09-18 13:59:51 +0800

    修改了数据库字段类型

--- model/usersDAO.js
+++ model/usersDAO.js
@@ -9,6 +9,17 @@ class DB {
     getUserheadpic(userid){
         return DAO("select headpic from users where userid  = ?",[userid])
     }
-
+    //获取用户个性签名
+    getUserautograph(userid){
+        return DAO("select autograph from users where userid  = ?",[userid])
+    }
+    //获取用户收藏的全部文章
+    getUserCollections(userid){
+        return DAO("select title,time from collections,artical where collections.articalid = artical.articalid and collections.userid = ?",[userid])
+    }
+    //增加收藏文章
+    AddCollections(){
+        return DAO("insert into collections( articalid ,time) values (?,?)",[userid])
+    }
 }
 module.exports = new  DB();
