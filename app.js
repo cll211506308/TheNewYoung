5b001bda5c5906e195953f3438971efdf2b9aa32
asus <578690086@qq.com>
2018-10-13 14:15:43 +0800

    设置响应头

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ const friends = require('./routes/friends')
 // error handler
 onerror(app)
 
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', ctx.headers.origin); // 很奇怪的是，使用 * 会出现一些其他问题
+    ctx.set('Access-Control-Allow-Headers', 'content-type');
+    ctx.set('Access-Control-Allow-Methods', 'OPTIONS,GET,HEAD,PUT,POST,DELETE,PATCH')
+    await next();
+});
+
 // middlewares
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
